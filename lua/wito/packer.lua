7e1e14a06d7357a4391bf0d3c7a300ee03f77115
Witold Zolnowski <blue3656@gmail.com>
2023-06-08 08:12:06 +0100

    add terminal plugin

--- lua/wito/packer.lua
+++ lua/wito/packer.lua
@@ -62,7 +62,11 @@ return require('packer').startup(function(use)
       require('Comment').setup()
     end
   }
-  use 'sindrets/diffview.nvim'
+
+  -- Terminal
+  use { "akinsho/toggleterm.nvim", tag = '*', config = function()
+    require("toggleterm").setup()
+  end }
 
   --File browser
   use {
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -110,11 +110,6 @@ _G.packer_plugins = {
     path = "/home/witold/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
-  ["diffview.nvim"] = {
-    loaded = true,
-    path = "/home/witold/.local/share/nvim/site/pack/packer/start/diffview.nvim",
-    url = "https://github.com/sindrets/diffview.nvim"
-  },
   ["friendly-snippets"] = {
     loaded = true,
     path = "/home/witold/.local/share/nvim/site/pack/packer/start/friendly-snippets",
@@ -208,6 +203,12 @@ _G.packer_plugins = {
     path = "/home/witold/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
+  ["toggleterm.nvim"] = {
+    config = { "\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15toggleterm\frequire\0" },
+    loaded = true,
+    path = "/home/witold/.local/share/nvim/site/pack/packer/start/toggleterm.nvim",
+    url = "https://github.com/akinsho/toggleterm.nvim"
+  },
   undotree = {
     loaded = true,
     path = "/home/witold/.local/share/nvim/site/pack/packer/start/undotree",
@@ -216,6 +217,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: toggleterm.nvim
+time([[Config for toggleterm.nvim]], true)
+try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15toggleterm\frequire\0", "config", "toggleterm.nvim")
+time([[Config for toggleterm.nvim]], false)
 -- Config for: Comment.nvim
 time([[Config for Comment.nvim]], true)
 try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
